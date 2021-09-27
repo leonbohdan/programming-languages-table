85eb5e41c466647046ec7a15f280648a36a9b86e
bohdan.leonets <bohdan.leonets@ria.com>
2021-09-27 17:07:54 +0300

    method POST was added

--- server/services/programmingLanguages.js
+++ server/services/programmingLanguages.js
@@ -2,6 +2,15 @@ const db = require('./db');
 const helper = require('../helper');
 const config = require('../config');
 
+// language for testing
+// const programmingLanguage = {
+//     name: 'newLang',
+//     released_year: '2021',
+//     githut_rank: '87',
+//     pypl_rank: '2',
+//     tiobe_rank: '5'
+// };
+
 async function getMultiple(page = 1){
     const offset = helper.getOffset(page, config.listPerPage);
 
@@ -20,6 +29,25 @@ async function getMultiple(page = 1){
     }
 };
 
+async function create(programmingLanguage) {
+    const result = await db.query(
+        `INSERT INTO programming_languages 
+        (name, released_year, githut_rank, pypl_rank, tiobe_rank) 
+        VALUES ${programmingLanguage.name},${programmingLanguage.released_year},
+        ${programmingLanguage.githut_rank},${programmingLanguage.pypl_rank},
+        ${programmingLanguage.tiobe_rank}`
+    );
+
+    let message = 'Error in creating programming language';
+
+    if (result.affectedRows) {
+        message = 'Programming language created successfully';
+    }
+
+    return {message};
+}
+
 module.exports = {
-    getMultiple
+    getMultiple,
+    create
 };
